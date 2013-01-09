9fc8b7e798f9efb8621428c168faf9aea69b655c
Matt Sanders <matt@modal.org>
2013-01-09 15:54:18 -0600

    Comment current tests; clean test_helper

--- test/integration/custom_test.rb
+++ test/integration/custom_test.rb
@@ -1,29 +1,29 @@
-require 'test_helper'
-
-class CustomTest < ActiveSupport::IntegrationCase
-
-  test 'controller access' do
-    visit custom_path
-
-    assert_equal 1, counters['custom.visits']
-    assert_equal 3, counters['custom.events']
-
-    assert_equal 12, aggregate['custom.timing'][:sum]
-    assert_equal 2, aggregate['custom.timing'][:count]
-  end
-
-  test 'model class access' do
-    visit custom_path
-
-    assert_equal 3, counters['custom.model.lookups']
-    assert_equal 19.0, aggregate['custom.model.search'][:sum]
-    assert_equal 2, aggregate['custom.model.search'][:count]
-  end
-
-  test 'model instance access' do
-    visit custom_path
-
-    assert_equal 1, counters['custom.model.touch']
-  end
-
-end
+# require 'test_helper'
+# 
+# class CustomTest < ActiveSupport::IntegrationCase
+# 
+#   test 'controller access' do
+#     visit custom_path
+# 
+#     assert_equal 1, counters['custom.visits']
+#     assert_equal 3, counters['custom.events']
+# 
+#     assert_equal 12, aggregate['custom.timing'][:sum]
+#     assert_equal 2, aggregate['custom.timing'][:count]
+#   end
+# 
+#   test 'model class access' do
+#     visit custom_path
+# 
+#     assert_equal 3, counters['custom.model.lookups']
+#     assert_equal 19.0, aggregate['custom.model.search'][:sum]
+#     assert_equal 2, aggregate['custom.model.search'][:count]
+#   end
+# 
+#   test 'model instance access' do
+#     visit custom_path
+# 
+#     assert_equal 1, counters['custom.model.touch']
+#   end
+# 
+# end
