c71d710919d98bd25c6313bb6247803863eb0b48
Matt Sanders <matt@modal.org>
2013-01-21 12:04:26 -0600

    Verify log level behavior

--- test/unit/rack/logger_test.rb
+++ test/unit/rack/logger_test.rb
@@ -1,12 +1,13 @@
 require 'test_helper'
+require 'stringio'
 
 module Librato
   class Rack
     class LoggerTest < MiniTest::Unit::TestCase
 
       def setup
-        @read_buffer, write = IO.pipe
-        log_object = ::Logger.new(write) # stdlib logger
+        @buffer = StringIO.new
+        log_object = ::Logger.new(@buffer) # stdlib logger
         @logger = Logger.new(log_object) # rack logger
       end
 
@@ -20,6 +21,33 @@ module Librato
         assert_equal :trace, @logger.log_level, 'should accept strings'
 
         assert_raises(InvalidLogLevel) { @logger.log_level = :foo }
+      end
+
+      def test_simple_logging
+        @logger.log_level = :info
+
+        # logging at log level
+        @logger.log :info, 'a log message'
+        @buffer.rewind
+        lines = @buffer.readlines
+        assert_equal 1, buffer_lines.length, 'should have added a line'
+        assert buffer_lines[0].index('a log message'), 'should log message'
+
+        # logging above level
+        @logger.log :error, 'an error message'
+        assert_equal 2, buffer_lines.length, 'should have added a line'
+        assert buffer_lines[1].index('an error message'), 'should log message'
+
+        # logging below level
+        @logger.log :debug, 'a debug message'
+        assert_equal 2, buffer_lines.length, 'should not have added a line'
+      end
+
+      private
+
+      def buffer_lines
+        @buffer.rewind
+        @buffer.readlines
       end
 
     end
