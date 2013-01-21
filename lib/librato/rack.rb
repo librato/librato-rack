9865d195052be2098efab1c3c94b91a14b884957
Matt Sanders <matt@modal.org>
2013-01-21 14:33:07 -0600

    Set up default collector through Librato global

--- lib/librato/rack.rb
+++ lib/librato/rack.rb
@@ -2,41 +2,53 @@ require 'thread'
 require 'librato/metrics'
 
 module Librato
+  extend SingleForwardable
+  def_delegators :collector, :increment, :measure, :timing, :group
+
+  def self.collector
+    @collector ||= Librato::Collector.new
+  end
+end
+
+module Librato
   # Middleware for rack applications. Installs tracking hearbeat for
   # metric submission and tracks performance metrics.
   #
-  # @example
+  # @example A basic rack app
   #   require 'rack'
   #   require 'librato-rack'
   #
   #   app = Rack::Builder.app do
   #     use Librato::Rack
-  #     run lambda { |env| }
+  #     run lambda { |env| [200, {"Content-Type" => 'text/html'}, ["Hello!"]]}
   #   end
   #
   class Rack
+    DEFAULT_TRACKER = Librato
 
-    def initialize(app, config = nil)
+    def initialize(app, config = {})
       @app, @config = app, config
     end
 
     def call(env)
       # @metrics.check_worker
-
-      header_metrics env
+      record_header_metrics(env)
 
       time     = Time.now
       response = @app.call(env)
       duration = (Time.now - time) * 1000.0
-
-      request_metrics response.first, duration
+      record_request_metrics(response.first, duration)
 
       response
     end
 
     private
 
-    def header_metrics(env)
+    def tracker
+      @tracker ||= @config[:tracker] || DEFAULT_TRACKER
+    end
+
+    def record_header_metrics(env)
       return unless env.keys.include?('HTTP_X_HEROKU_QUEUE_DEPTH')
 
       # @metrics.group 'rack.heroku' do |group|
@@ -46,20 +58,20 @@ module Librato
       # end
     end
 
-    def request_metrics(status, duration)
-      # @metrics.group 'rack.request' do |group|
-      #   group.increment 'total'
-      #   group.timing    'time', duration
-      #   group.increment 'slow' if duration > 200.0
-      #
-      #   group.group 'status' do |s|
-      #     s.increment status
-      #     s.increment "#{status.to_s[0]}xx"
-      #
-      #     s.timing "#{status}.time", duration
-      #     s.timing "#{status.to_s[0]}xx.time", duration
-      #   end
-      # end
+    def record_request_metrics(status, duration)
+      tracker.group 'rack.request' do |group|
+        group.increment 'total'
+        group.timing    'time', duration
+        # group.increment 'slow' if duration > 200.0
+
+        # group.group 'status' do |s|
+        #   s.increment status
+        #   s.increment "#{status.to_s[0]}xx"
+        #
+        #   s.timing "#{status}.time", duration
+        #   s.timing "#{status.to_s[0]}xx.time", duration
+        # end
+      end
     end
 
   end
@@ -70,4 +82,5 @@ require 'librato/rack/configuration'
 require 'librato/rack/errors'
 require 'librato/rack/logger'
 require 'librato/rack/worker'
+require 'librato/rack/validating_queue'
 require 'librato/rack/version'
---
+++ test/integration/request_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+require 'rack/test'
+
+# Tests for universal tracking for all request paths
+#
+class RequestTest < MiniTest::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Rack::Builder.parse_file('test/apps/basic.ru').first
+  end
+
+  def test_increment_total
+    get '/'
+    assert last_response.ok?
+
+    assert_equal 1, counters["rack.request.total"]
+    # assert_equal 1, counters["rack.request.status.200"]
+    # assert_equal 1, counters["rack.request.status.2xx"]
+  end
+
+  private
+
+  def counters
+    Librato.collector.counters
+  end
+
+  # def test_increment_status
+  #   get '/status/204'
+  #
+  #   assert_equal 2, counters["rails.request.total"]
+  #   assert_equal 1, counters["rails.request.status.200"]
+  #   assert_equal 1, counters["rails.request.status.204"]
+  #   assert_equal 2, counters["rails.request.status.2xx"]
+  # end
+  #
+  #   test 'request times' do
+  #     visit root_path
+  #
+  #     # common for all paths
+  #     assert_equal 1, aggregate["rails.request.time"][:count], 'should record total time'
+  #     assert_equal 1, aggregate["rails.request.time.db"][:count], 'should record db time'
+  #     assert_equal 1, aggregate["rails.request.time.view"][:count], 'should record view time'
+  #
+  #     # status specific
+  #     assert_equal 1, aggregate["rails.request.status.200.time"][:count]
+  #     assert_equal 1, aggregate["rails.request.status.2xx.time"][:count]
+  #   end
+  #
+  #   test 'track exceptions' do
+  #     begin
+  #       visit exception_path #rescue nil
+  #     rescue RuntimeError => e
+  #       raise unless e.message == 'test exception!'
+  #     end
+  #     assert_equal 1, counters["rails.request.exceptions"]
+  #   end
+  #
+  #   test 'track slow requests' do
+  #     visit slow_path
+  #     assert_equal 1, counters["rails.request.slow"]
+  #   end
+
+end
