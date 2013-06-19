96c1b2e64d96d41e77a560558ab6852bd0652f33
Matt Sanders <matt@modal.org>
2013-06-19 16:39:47 -0700

    Clean up test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,20 +3,6 @@ Bundler.setup
 
 require 'pry'
 require 'minitest/autorun'
-# require 'mocha/setup'
-
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
 
 require 'librato/rack'
 
-# require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-# require "rails/test_help"
-
-# Configure capybara
-# require 'capybara/rails'
-# Capybara.default_driver = :rack_test
-# Capybara.default_selector = :css
-
-# Load support files
-#Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
