2e5dee74bc1c012397aead0b407b9f1388053041
Matt Sanders <matt@modal.org>
2013-08-13 14:47:08 -0700

    Ensure all options passed to grouped increment are respected

--- test/unit/collector/group_test.rb
+++ test/unit/collector/group_test.rb
@@ -8,8 +8,10 @@ module Librato
         collector = Collector.new
         collector.group 'foo' do |g|
           g.increment :bar
+          g.increment :baz, :source => 'bang'
         end
         assert_equal 1, collector.counters['foo.bar']
+        assert_equal 1, collector.counters.fetch('foo.baz', source: 'bang')
       end
 
       def test_measure
